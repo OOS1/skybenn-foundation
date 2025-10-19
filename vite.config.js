6ede6b76b790d3b47cc8e2250b0d91a27e05a70f
skybenn <benskysain@gmail.com>
2025-10-19 02:51:53 -0400

    added path command

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/skybenn-foundation/',  // must match repo name exactly (case-sensitive)
+  base: '/skybenn-foundation/',   // must match repo name exactly (case-sensitive)
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        programs: resolve(__dirname, 'programs.html'),
+        donate: resolve(__dirname, 'donate.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      }
+    }
+  }
 })
